e0e9d0c23a9f5b6b37eb2b315d9fa5dc1e3b5349
09liweis <weisen.li@hotmail.com>
2025-06-21 21:21:21 -0400

    add status to creditRecord

--- src/lib/types/credit.ts
+++ src/lib/types/credit.ts
@@ -1,6 +1,7 @@
 export interface CreditRecord {
   amount: number, //+100, -100
   tp: string, //usage: topup, staging, cleaning
+  status: string, //pending, done
   stripe_client_secret: string,
   id?: string,
   user_id: string
