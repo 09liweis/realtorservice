46433c2b85803ec16cb827d7c3ea85584dd37759
Sam Li <weisen.li@hotmail.com>
2025-08-05 03:47:11 +0000

    add listingpage component

---
+++ src/routes/realtor-services/listing/ListingPage.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let pageTitle;
+  export let pageContent;
+</script>
+
+<h1 class="text-3xl font-bold text-gray-900 mb-6">{pageTitle}</h1>
+
+<div class="prose max-w-none">
+  <p>
+    {pageContent}
+  </p>
+
+  <p class="mt-6 font-medium">
+    Got a listing? Just call us at <a
+      href="tel:416-800-0688"
+      class="text-primary hover:underline">416-800-0688</a
+    >
+  </p>
+</div>
