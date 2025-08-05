1d10eed3f64a96949faa7378f9fe89a97b6f031e
Sam Li <weisen.li@hotmail.com>
2025-08-05 14:20:28 +0000

    update photographing service pag

--- src/routes/realtor-services/listing/photographing/+page.svelte
+++ src/routes/realtor-services/listing/photographing/+page.svelte
@@ -1,11 +1,12 @@
 <script>
   // Page metadata
   import { page } from "$app/stores";
-  import ListingPage from "../ListingPage.svelte";
+  import ServicePage from "../../../../lib/components/common/ServicePage.svelte";
 </script>
 
 <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-  <ListingPage
+  <ServicePage
+    callText="Ready to shine?"
     pageTitle="Photographing & Media Services"
     pageContent="Great visuals make all the difference. As part of our all-in-one Listing Service, our Photographing & Media Services are designed to showcase every property in its best light—literally. From stunning photography with blue sky enhancement to virtual staging, cinematic video tours, 3D walkthroughs, drone footage, floor plans, and custom property websites, we provide everything you need to make your listings stand out and sell faster."
   />
