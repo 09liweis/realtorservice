bfd0e25a7281451ecd7976d79514a6917afbcf4a
Sam Li <weisen.li@hotmail.com>
2025-08-05 14:19:37 +0000

    update painting service page

--- src/routes/realtor-services/listing/painting/+page.svelte
+++ src/routes/realtor-services/listing/painting/+page.svelte
@@ -1,11 +1,12 @@
 <script>
   // Page metadata
   import { page } from "$app/stores";
-  import ListingPage from "../ListingPage.svelte";
+  import ServicePage from "../../../../lib/components/common/ServicePage.svelte";
 </script>
 
 <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-  <ListingPage
+  <ServicePage
+    callText="A job to be done?"
     pageTitle="Professional Painting Service"
     pageContent="First impressions matter—especially in real estate. As part of our all-in-one Listing Service, our Painting & Renovation solutions are designed to refresh and elevate your property’s appeal before it hits the market. Whether it’s a quick touch-up or a full refresh, our team ensures every space looks clean, modern, and move-in ready—helping you attract more buyers and maximize value."
   />
