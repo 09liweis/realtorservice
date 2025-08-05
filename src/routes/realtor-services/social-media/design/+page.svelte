ee53f47718faa57448356f259d4ce148ab62025d
Sam Li <weisen.li@hotmail.com>
2025-08-05 14:30:17 +0000

    add design page

---
+++ src/routes/realtor-services/social-media/design/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  // Page metadata
+  import { page } from "$app/stores";
+  import ServicePage from "$lib/components/common/ServicePage.svelte";
+</script>
+
+<main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+  <ServicePage
+    callText="Design with taste and professionalism!"
+    pageTitle="Logo/Business Card/Website Design Service"
+    pageContent="Your brand starts here. As part of our Social Media & Branding services, we offer custom Logo Design, Business Cards, and Realtor Website Design tailored to reflect your unique style and professionalism. Whether you're launching your brand or refreshing your look, we create cohesive, eye-catching designs that make a lasting impression—both online and offline."
+  />
+</main>
