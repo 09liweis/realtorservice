38ec579e6243a5454fb89a9d31a9f5d806f6b8ee
Sam Li <weisen.li@hotmail.com>
2025-08-05 14:17:22 +0000

    update listing service pag

--- src/routes/realtor-services/listing/+page.svelte
+++ src/routes/realtor-services/listing/+page.svelte
@@ -1,11 +1,12 @@
 <script>
   // Page metadata
   import { page } from "$app/stores";
-  import ListingPage from "./ListingPage.svelte";
+  import ServicePage from "$lib/components/common/ServicePage.svelte";
 </script>
 
 <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-  <ListingPage
+  <ServicePage
+    callText="Got a listing?"
     pageTitle="Listing Service"
     pageContent="At REALTORSERVICE.CA, we make your listings shine. Our all-in-one Listing Service is designed to help Realtors present properties at their absolute best—quickly, professionally, and hassle-free. From painting and cleaning to staging, photography, video tours, and custom property websites, we handle every detail so you can list with confidence and sell with impact."
   />
