9bfb2e900343608a134a64ccc7b223cb81867ee7
Sam Li <weisen.li@hotmail.com>
2025-08-05 14:21:26 +0000

    update single post page

--- src/routes/realtor-services/listing/single-post/+page.svelte
+++ src/routes/realtor-services/listing/single-post/+page.svelte
@@ -1,11 +1,12 @@
 <script>
   // Page metadata
   import { page } from "$app/stores";
-  import ListingPage from "../ListingPage.svelte";
+  import ServicePage from "$lib/components/common/ServicePage.svelte";
 </script>
 
 <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
-  <ListingPage
+  <ServicePage
+    callText="For Sale or For Lease?"
     pageTitle="Sign Post Installation"
     pageContent="Make a strong statement from the curb. As part of our complete Listing Service, our Sign Post Installation ensures your property gets noticed from the moment buyers arrive. We provide professional, sturdy, and well-placed signage that highlights your listing and enhances your brand—installed quickly and securely, every time."
   />
