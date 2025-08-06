cd47009bac42f3c0850b4a223e477adb0421b792
09liweis <weisen.li@hotmail.com>
2025-08-06 11:32:01 -0400

    cleaning api post

--- src/routes/api/cleanings/+server.ts
+++ src/routes/api/cleanings/+server.ts
@@ -34,3 +34,37 @@ export const GET: RequestHandler = async ({ request }) => {
     );
   }
 };
+
+
+export const POST: RequestHandler = async ({ request }) => {
+  try {
+    const authUser = await checkAuth(request);
+    const user_id = authUser?.user_id || authUser?.id;
+    const isAdmin = authUser.isAdmin;
+    if (!user_id) {
+      return json({stats: 401, error: 'Unauthorized'});
+    }
+
+    if (isAdmin) {
+      return json({stats: 401, error: 'Admin can not do this operation'});
+    }
+
+    const cleaningService = await request.json();
+
+    const { data, error } = await supabase.from('cleanings').insert({...cleaningService,user_id});
+
+    if (error) {
+      return json(
+        { error: "Failed to add user cleaning services from Supabase" },
+        { status: 500 }
+      );
+    }
+    return json({ cleaning: data });
+  } catch (error) {
+    console.error("cleaning service insert Error: ", error);
+    return json(
+      { error: "Server error" },
+      { status: 500 }
+    );
+  }
+};
