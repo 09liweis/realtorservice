8e151675ff6eb6448e03a915d4dcd4774b0127ca
09liweis <weisen.li@hotmail.com>
2025-08-07 09:41:03 -0400

    offer property crud api

---
+++ src/routes/api/offerproperties/[propertyId]/+server.ts
@@ -0,0 +1,122 @@
+import { json } from "@sveltejs/kit";
+import supabase from "$lib/db/client";
+import type { RequestHandler } from "./$types";
+import { checkAuth } from "$lib/server/apiAuth";
+
+export const GET: RequestHandler = async ({ request,params }) => {
+  try {
+    const propertyId = params.propertyId
+    const authUser = await checkAuth(request);
+    const user_id = authUser?.user_id || authUser?.id;
+    if (!user_id) {
+      return json({stats: 401, error: 'Unauthorized'});
+    }
+
+    if (!propertyId) {
+      return json({stats: 404, error: 'Offer property not found'});
+    }
+
+    // Fetch user email from Supabase
+    const { data, error } = await supabase
+    .from('offer_properties')
+    .select('*')
+    .eq("user_id",user_id)
+    .eq("id", propertyId)
+    .single();
+    
+
+    if (error) {
+      return json(
+        { error: "Failed to fetch user offer_properties from Supabase" },
+        { status: 500 }
+      );
+    }
+    return json({ offer_properties: data });
+  } catch (error) {
+    console.error("offer_properties Error: ", error);
+    return json(
+      { error: "Server error" },
+      { status: 500 }
+    );
+  }
+};
+
+
+export const PUT: RequestHandler = async ({ request,params }) => {
+  try {
+    const propertyId = params.propertyId
+    const authUser = await checkAuth(request);
+    const user_id = authUser?.user_id || authUser?.id;
+    if (!user_id) {
+      return json({stats: 401, error: 'Unauthorized'});
+    }
+
+    if (!propertyId) {
+      return json({stats: 404, error: 'Openhouse not found'});
+    }
+
+    const offerProperty = await request.json();
+
+    // Fetch user email from Supabase
+    const { data, error } = await supabase
+    .from('offer_properties')
+    .update(offerProperty)
+    .eq('id', propertyId)
+    .eq("user_id", user_id)
+    
+
+    if (error) {
+      return json(
+        { error: "Failed to update user openhouse from Supabase" },
+        { status: 500 }
+      );
+    }
+    return json({ openhouse: data });
+  } catch (error) {
+    console.error("openhouse Error: ", error);
+    return json(
+      { error: "Server error" },
+      { status: 500 }
+    );
+  }
+};
+
+
+export const DELETE: RequestHandler = async ({ request,params }) => {
+  try {
+    const propertyId = params.propertyId
+    const authUser = await checkAuth(request);
+    const user_id = authUser?.user_id || authUser?.id;
+    if (!user_id) {
+      return json({stats: 401, error: 'Unauthorized'});
+    }
+
+    if (!propertyId) {
+      return json({stats: 404, error: 'Offer property not found'});
+    }
+
+    // Fetch user email from Supabase
+    
+    const { data, error } = await supabase
+    .from('offers')
+    .delete()
+    .eq("user_id",user_id)
+    .eq("property_id", propertyId);
+
+    await supabase.from('offer_properties').delete().eq("user_id", user_id).eq("id",propertyId);
+
+    if (error) {
+      return json(
+        { error: "Failed to delete user offers and property from Supabase" },
+        { status: 500 }
+      );
+    }
+    return json({ msg: 'Deleted' });
+  } catch (error) {
+    console.error("offers Error: ", error);
+    return json(
+      { error: "Server error" },
+      { status: 500 }
+    );
+  }
+};
