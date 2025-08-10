178da26e515822afa95665aff27d94261e57434a
Siri Yeung <491763280@qq.com>
2025-08-10 13:34:23 +0000

    add photographing service image

--- src/routes/realtor-services/listing/photographing/+page.svelte
+++ src/routes/realtor-services/listing/photographing/+page.svelte
@@ -6,6 +6,7 @@
 
 <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
   <ServicePage
+    serviceImage='https://images.pexels.com/photos/4549411/pexels-photo-4549411.jpeg?auto=compress&cs=tinysrgb&w=800'
     callText="Ready to shine?"
     pageTitle="Photographing & Media Services"
     pageContent="Great visuals make all the difference. As part of our all-in-one Listing Service, our Photographing & Media Services are designed to showcase every property in its best light—literally. From stunning photography with blue sky enhancement to virtual staging, cinematic video tours, 3D walkthroughs, drone footage, floor plans, and custom property websites, we provide everything you need to make your listings stand out and sell faster."
