6905cc257c0b9aaf5a1367298a0d3840dae25c5e
Siri Yeung <491763280@qq.com>
2025-08-10 13:35:45 +0000

    add service image to sign-post page

--- src/routes/realtor-services/listing/sign-post/+page.svelte
+++ src/routes/realtor-services/listing/sign-post/+page.svelte
@@ -6,6 +6,7 @@
 
 <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
   <ServicePage
+    serviceImage='https://images.pexels.com/photos/8470803/pexels-photo-8470803.jpeg?auto=compress&cs=tinysrgb&w=800'
     callText="For Sale or For Lease?"
     pageTitle="Sign Post Installation"
     pageContent="Make a strong statement from the curb. As part of our complete Listing Service, our Sign Post Installation ensures your property gets noticed from the moment buyers arrive. We provide professional, sturdy, and well-placed signage that highlights your listing and enhances your brand—installed quickly and securely, every time."
