22d9793ffc6e22d0b8c9d8046b6b5598c397d553
Siri Yeung <491763280@qq.com>
2025-08-10 13:39:43 +0000

    add social media service image

--- src/routes/realtor-services/social-media/management/+page.svelte
+++ src/routes/realtor-services/social-media/management/+page.svelte
@@ -7,6 +7,7 @@
 
 <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
   <ServicePage
+    serviceImage='https://images.pexels.com/photos/267350/pexels-photo-267350.jpeg?auto=compress&cs=tinysrgb&w=800'
     callText="You are way more popular than you think!"
     pageTitle="Social Media Account Management"
     pageContent="Let us handle your socials—so you can focus on selling. As part of our Social Media & Branding services, our Social Media Account Management takes the stress out of maintaining an online presence. We operate your accounts on your behalf, posting engaging content, responding to interactions, and keeping your brand active and professional across all platforms. Consistency, creativity, and strategy—all done for you."
