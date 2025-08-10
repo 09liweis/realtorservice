4e2290b893d1acb3a8e6f263615d5129936a84a7
Siri Yeung <491763280@qq.com>
2025-08-10 13:38:23 +0000

    add web design service image

--- src/routes/realtor-services/social-media/design/+page.svelte
+++ src/routes/realtor-services/social-media/design/+page.svelte
@@ -6,6 +6,7 @@
 
 <main class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
   <ServicePage
+    serviceImage='https://images.pexels.com/photos/326514/pexels-photo-326514.jpeg?auto=compress&cs=tinysrgb&w=800'
     callText="Design with taste and professionalism!"
     pageTitle="Logo/Business Card/Website Design Service"
     pageContent="Your brand starts here. As part of our Social Media & Branding services, we offer custom Logo Design, Business Cards, and Realtor Website Design tailored to reflect your unique style and professionalism. Whether you're launching your brand or refreshing your look, we create cohesive, eye-catching designs that make a lasting impression—both online and offline."
