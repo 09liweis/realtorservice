12ce7c2a3c606ba503b9e700643a97975a31dfee
Siri Yeung <491763280@qq.com>
2025-09-28 13:31:25 +0000

    add stagings service to admin stagins page

--- src/routes/dashboard/stagings/+page.svelte
+++ src/routes/dashboard/stagings/+page.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
   import { StagingScreen } from "$lib/components/stagings";
+    import { getPageTitle } from "$lib/types/constant";
 </script>
+
+<svelte:head>
+  <title>{getPageTitle('Stagings Services')}</title>
+</svelte:head>
 
 <StagingScreen />
