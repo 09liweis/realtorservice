69f48d0440c9a9a33ed0df140b09d7fab1a1314e
Sam Li <weisen.li@hotmail.com>
2025-10-04 11:27:23 +0000

    update formatDate month to long

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -33,7 +33,7 @@ export function formatDate(date: string|undefined) {
   if (!date) return '';
   return new Date(date).toLocaleDateString('en-US', {
     year: 'numeric',
-    month: 'short',
+    month: 'long',
     day: 'numeric'
   });
 }
