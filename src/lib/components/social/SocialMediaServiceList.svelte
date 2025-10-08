ea7d81e4a970020870b3cd5b65c91c28d615776a
Siri Yeung <491763280@qq.com>
2025-10-08 14:45:53 +0000

    remove style from socialmediaservcielist

--- src/lib/components/social/SocialMediaServiceList.svelte
+++ src/lib/components/social/SocialMediaServiceList.svelte
@@ -49,12 +49,3 @@
     {/each}
   </div>
 {/if}
-
-<style>
-  .line-clamp-3 {
-    display: -webkit-box;
-    -webkit-line-clamp: 3;
-    -webkit-box-orient: vertical;
-    overflow: hidden;
-  }
-</style>
