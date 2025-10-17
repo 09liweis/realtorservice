f19c20cf29102592db034639501bb644a218a09a
SiriYeung <119325339+SiriYeung@users.noreply.github.com>
2025-10-17 04:14:04 +0000

    remove duplicate cleaning request button

--- src/lib/components/cleanings/CleaningList.svelte
+++ src/lib/components/cleanings/CleaningList.svelte
@@ -59,24 +59,6 @@
       <p class="mt-1 text-sm text-gray-500">
         Get started by creating a new cleaning request.
       </p>
-      <div class="mt-6" in:fade={{ delay: 300, duration: 300 }}>
-        <Button
-          onclick={() => dispatch("new")}
-        >
-          <svg
-            class="mr-2 -ml-1 h-5 w-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          New Cleaning Request
-        </Button>
-      </div>
     </div>
   {:else}
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
